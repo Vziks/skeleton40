cdc510710271a962f15bdea9086d3c397e35b814
kolyadin <aleksey.kolyadin@isobar.ru>
2019-02-19 18:32:17 +0300

    composer update env files update test setup update docker-compose update

--- bin/setup-test.sh
+++ bin/setup-test.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-if [ ! -f phpunit.xml ]; then
+if [[ ! -f phpunit.xml ]]; then
     cp phpunit.xml.dist phpunit.xml
 fi
 
-php bin/console doctrine:database:drop --force -e test
-php bin/console doctrine:database:create --if-not-exists -e test
-php bin/console doctrine:schema:drop --force -e test
-php bin/console doctrine:migrations:migrate --no-interaction -e test
-php bin/console doctrine:fixtures:load -n -e test
-#php app/console sonata:media:fix-media-context -e test
-php bin/console assets:install --symlink web -e test
+composer install --prefer-dist -n -o
+
+php bin/console doctrine:database:create -e test --if-not-exists
+php bin/console doctrine:migrations:migrate -e test --no-interaction
+php bin/console doctrine:fixtures:load -e test -n
+
+echo "Application is ready for testing" >&2
