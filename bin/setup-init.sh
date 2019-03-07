cf0def6deecc7e4aa3ae4976e3ca51d5fa74fb64
kolyadin <aleksey.kolyadin@isobar.ru>
2019-03-07 15:20:57 +0300

    add Makefile improve setup scripts

--- bin/setup-init.sh
+++ bin/setup-init.sh
@@ -4,10 +4,14 @@ if [[ ! -f phpunit.xml ]]; then
     cp phpunit.xml.dist phpunit.xml
 fi
 
+if [[ ! -f .env.local ]]; then
+    cp .env .env.local
+fi
+
 composer install --prefer-dist -n -o
 
 php bin/console doctrine:database:create --if-not-exists
 php bin/console doctrine:migrations:migrate --no-interaction
 php bin/console doctrine:fixtures:load -n
 
-echo "Application is ready for work" >&2
+echo "Application is ready for work"
--- bin/setup-test.sh
+++ bin/setup-test.sh
@@ -4,10 +4,10 @@ if [[ ! -f phpunit.xml ]]; then
     cp phpunit.xml.dist phpunit.xml
 fi
 
-composer install --prefer-dist -n -o
+composer install --prefer-dist --no-interaction --optimize-autoloader --no-scripts
 
 php bin/console doctrine:database:create -e test --if-not-exists
 php bin/console doctrine:migrations:migrate -e test --no-interaction
 php bin/console doctrine:fixtures:load -e test -n
 
-echo "Application is ready for testing" >&2
+echo "Application is ready for testing"
